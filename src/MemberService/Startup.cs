2408105708be8e10a491069e3ecab632e4bc5434
Erik Lydersen <erikly@gmail.com>
2019-02-02 17:06:47 +0100

    Added auto migrations on startup

--- src/MemberService/Startup.cs
+++ src/MemberService/Startup.cs
@@ -46,7 +46,7 @@ namespace MemberService
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext context)
         {
             if (env.IsDevelopment())
             {
@@ -58,6 +58,11 @@ namespace MemberService
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+
+            if (!context.Database.EnsureCreated())
+            {
+                context.Database.Migrate();
             }
 
             app.UseHttpsRedirection();
