1d6c905e94bdddb1fff5f152dbef967cc87d79e7
Marius Gundersen <me@mariusgundersen.net>
2019-02-16 21:21:44 +0100

    Implemented email sending using SMTP and tested it by adding email confirmation requirement

---
+++ src/MemberService/Configs/EmailConfig.cs
@@ -0,0 +1,15 @@
+namespace MemberService.Configs
+{
+    public class EmailConfig
+    {
+        public string From { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+    }
+}
