4bda9319486d4dbf369220b82282ed41c7b66892
Marius Gundersen <me@mariusgundersen.net>
2019-04-22 15:00:38 +0200

    Show the PayedAtUtc DateTime in Oslo-time

--- src/MemberService/Services/Extensions.cs
+++ src/MemberService/Services/Extensions.cs
@@ -1,3 +1,4 @@
+using NodaTime;
 using System;
 using System.Linq.Expressions;
 
@@ -5,9 +6,19 @@ namespace MemberService.Services
 {
     public static class Extensions
     {
-        public static string FormatMoney(this decimal amount) => string.Format("kr {0:0},-", amount);
+        public static string FormatMoney(this decimal amount)
+            => string.Format("kr {0:0},-", amount);
+
+        public static string ToOsloDate(this DateTime utc)
+            => Instant.FromDateTimeUtc(utc.WithKind(DateTimeKind.Utc)).InZone(Constants.TimeZoneOslo).Date.ToString();
+
+        public static string ToOsloDateTime(this DateTime utc)
+            => Instant.FromDateTimeUtc(utc.WithKind(DateTimeKind.Utc)).InZone(Constants.TimeZoneOslo).ToString();
 
         public static Expression<Func<T, bool>> Not<T>(this Expression<Func<T, bool>> predicate)
             => Expression.Lambda<Func<T, bool>>(Expression.Not(predicate.Body), predicate.Parameters);
+
+        private static DateTime WithKind(this DateTime dateTime, DateTimeKind kind)
+            => DateTime.SpecifyKind(dateTime, kind);
     }
 }
