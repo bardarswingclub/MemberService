73945f9c2b309aa8bcbafc23a3f5741f831832b3
Marius Gundersen <me@mariusgundersen.net>
2019-07-07 16:33:01 +0200

    Fixed second semester calculation

--- src/MemberService/Constants.cs
+++ src/MemberService/Constants.cs
@@ -9,6 +9,6 @@ namespace MemberService
 
         public static DateTime ThisYearUtc => new DateTime(DateTime.UtcNow.Year, 1, 1);
 
-        public static DateTime ThisSemesterUtc => new DateTime(DateTime.UtcNow.Year, (DateTime.UtcNow.Month / 7) * 7 + 1, 1);
+        public static DateTime ThisSemesterUtc => new DateTime(DateTime.UtcNow.Year, (DateTime.UtcNow.Month >= 7 ? 7 : 1), 1);
     }
 }
