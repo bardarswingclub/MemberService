87aad9438336734f0ef2743f470b5d639f1f2acf
Marius Gundersen <me@mariusgundersen.net>
2020-01-10 23:34:21 +0100

    Fixed spelling

--- src/MemberService/Pages/Home/SignupInputModel.cs
+++ src/MemberService/Pages/Home/SignupInputModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using MemberService.Pages.Shared;
 
 namespace MemberService.Pages.Home
@@ -9,7 +8,7 @@ namespace MemberService.Pages.Home
     {
         public DateTime SignupOpensAt { get; set; }
 
-        [DisplayName("Jeg godtar rettningslinjerne til Bårdar Swing Club")]
+        [DisplayName("Jeg godtar rettningslinjene til Bårdar Swing Club")]
         [EnforceTrue(ErrorMessage = "Du må godta våre rettningslinjer for å delta på kurs")]
         public bool Accept { get; set; }
     }
