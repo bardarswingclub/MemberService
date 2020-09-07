2fdd66e8bc770f075e78035ec9f398d8d66e1da9
Marius Gundersen <me@mariusgundersen.net>
2020-09-07 22:08:09 +0200

    Added migrations

---
+++ src/MemberService/Data/Migrations/20200907200643_AddIncludedInFee.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MemberService.Data.Migrations
+{
+    public partial class AddIncludedInFee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IncludedInClassesFee",
+                table: "EventSignupOptions",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IncludedInTrainingFee",
+                table: "EventSignupOptions",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IncludedInClassesFee",
+                table: "EventSignupOptions");
+
+            migrationBuilder.DropColumn(
+                name: "IncludedInTrainingFee",
+                table: "EventSignupOptions");
+        }
+    }
+}
