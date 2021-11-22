52d78d11458c3a34006d955f003a1709d5e05d7b
Marius Gundersen <me@mariusgundersen.net>
2021-11-22 10:33:50 +0100

    Improved view of events not part of semester

--- src/MemberService/Pages/Event/ParticipantsModel.cs
+++ src/MemberService/Pages/Event/ParticipantsModel.cs
@@ -4,10 +4,11 @@ namespace MemberService.Pages.Event
 {
     public class ParticipantsModel
     {
-        public ParticipantsModel(IReadOnlyList<EventSignupModel> signups, bool allowPartnerSignup)
+        public ParticipantsModel(IReadOnlyList<EventSignupModel> signups, bool allowPartnerSignup, bool showPriority)
         {
             Signups = signups;
             AllowPartnerSignup = allowPartnerSignup;
+            ShowPriority = showPriority;
         }
 
         public IReadOnlyList<EventSignupModel> Signups { get; }
@@ -15,5 +16,7 @@ namespace MemberService.Pages.Event
         public EventSignupModel this[int index] => Signups[index];
 
         public bool AllowPartnerSignup { get; }
+
+        public bool ShowPriority { get; }
     }
 }
