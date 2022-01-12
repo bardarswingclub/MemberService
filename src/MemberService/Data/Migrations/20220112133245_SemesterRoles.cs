45d0eea84e60c588f3669aeba2c68af41ac5a61b
Marius Gundersen <me@mariusgundersen.net>
2022-01-12 15:03:26 +0100

    Added migration

---
+++ src/MemberService/Data/Migrations/20220112133245_SemesterRoles.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MemberService.Data.Migrations
+{
+    public partial class SemesterRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SemesterRoles",
+                columns: table => new
+                {
+                    SemesterId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SemesterRoles", x => new { x.SemesterId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_SemesterRoles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SemesterRoles_Semesters_SemesterId",
+                        column: x => x.SemesterId,
+                        principalTable: "Semesters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SemesterRoles_UserId",
+                table: "SemesterRoles",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SemesterRoles");
+        }
+    }
+}
