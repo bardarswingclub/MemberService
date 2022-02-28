5577a0ececb7b0b02c60035a8deff872c3accb2e
Marius Gundersen <marius.gundersen@clave.no>
2022-02-28 10:30:58 +0100

    Some migrations that were needed

---
+++ src/MemberService/Data/Migrations/20220228092939_SurveyIndexes.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MemberService.Data.Migrations
+{
+    public partial class SurveyIndexes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Semesters_SurveyId",
+                table: "Semesters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_SurveyId",
+                table: "Events");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Semesters_SurveyId",
+                table: "Semesters",
+                column: "SurveyId",
+                unique: true,
+                filter: "[SurveyId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_SurveyId",
+                table: "Events",
+                column: "SurveyId",
+                unique: true,
+                filter: "[SurveyId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Semesters_SurveyId",
+                table: "Semesters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_SurveyId",
+                table: "Events");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Semesters_SurveyId",
+                table: "Semesters",
+                column: "SurveyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_SurveyId",
+                table: "Events",
+                column: "SurveyId");
+        }
+    }
+}
