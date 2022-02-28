b8ffe60ac9ec338730736c33b7c634bda4529565
Marius Gundersen <marius.gundersen@clave.no>
2022-02-28 10:34:47 +0100

    migrations

---
+++ src/MemberService/Data/Migrations/20220228093342_FriendlyName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MemberService.Data.Migrations
+{
+    public partial class FriendlyName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FriendlyName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FriendlyName",
+                table: "AspNetUsers");
+        }
+    }
+}
