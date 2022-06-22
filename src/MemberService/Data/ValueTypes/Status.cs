d87c002848ad409e42c92dc16e0d1004f60e3295
Marius Gundersen <marius.gundersen@clave.no>
2022-06-22 09:01:18 +0200

    Localize enums

--- src/MemberService/Data/ValueTypes/Status.cs
+++ src/MemberService/Data/ValueTypes/Status.cs
@@ -6,24 +6,24 @@ public enum Status
 {
     Unknown = 0,
 
-    [Display(Name = "Påmeldt", Description = "Påmeldt")]
+    [Display(Name = "Påmeldt", Description = "Påmeldt", ResourceType = typeof(Status))]
     Pending = 1,
 
-    [Display(Name = "Anbefalt plass", Description = "Påmeldt")]
+    [Display(Name = "Anbefalt plass", Description = "Påmeldt", ResourceType = typeof(Status))]
     Recommended = 2,
 
-    [Display(Name = "På venteliste", Description = "På venteliste")]
+    [Display(Name = "På venteliste", Description = "På venteliste", ResourceType = typeof(Status))]
     WaitingList = 3,
 
-    [Display(Name = "Gitt plass", Description = "Fått plass")]
+    [Display(Name = "Gitt plass", Description = "Fått plass", ResourceType = typeof(Status))]
     Approved = 4,
 
-    [Display(Name = "Godtatt og betalt", Description = "Deltar")]
+    [Display(Name = "Godtatt og betalt", Description = "Deltar", ResourceType = typeof(Status))]
     AcceptedAndPayed = 5,
 
-    [Display(Name = "Takket nei til plass", Description = "Takket nei")]
+    [Display(Name = "Takket nei til plass", Description = "Takket nei", ResourceType = typeof(Status))]
     RejectedOrNotPayed = 6,
 
-    [Display(Name = "Ikke fått plass", Description = "Ikke fått plass")]
+    [Display(Name = "Ikke fått plass", Description = "Ikke fått plass", ResourceType = typeof(Status))]
     Denied = 7
 }
