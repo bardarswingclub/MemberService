8ae53786fafff099d162e8f11fe0ff24362f7f03
Frederik Vestre <freqmod@gmail.com>
2025-01-12 18:02:53 +0000

    Use gmail and smtp for email sending

--- src/MemberService/Services/EmailService.cs
+++ src/MemberService/Services/EmailService.cs
@@ -1,23 +1,28 @@
 ﻿namespace MemberService.Services;
 
+using MimeKit;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
 using MemberService.Emails.Account;
 using MemberService.Emails.Event;
 
 using MemberService.Data;
 
-using SendGrid.Helpers.Mail;
 using Markdig;
 
 public class EmailService : IEmailService
 {
     private readonly IPartialRenderer _partialRenderer;
-    private readonly IEmailer _emailer;
+//    private readonly IEmailer _emailer;
+    private readonly IConfiguration _configuration;
 
     public EmailService(
-        IPartialRenderer partialRenderer, IEmailer emailer)
+        IPartialRenderer partialRenderer, IConfiguration configuration)
     {
         _partialRenderer = partialRenderer;
-        _emailer = emailer;
+        _configuration = configuration;
     }
 
     public async Task SendLoginEmail(string email, string name, LoginModel model)
@@ -26,19 +31,35 @@ public class EmailService : IEmailService
 
         var body = await _partialRenderer.RenderPartial("~/Emails/Account/Login.cshtml", model);
 
-        await _emailer.Send(
-            new EmailAddress(email, name),
+        await SendEmailAsync(
+            email, name,
             subject,
             body);
     }
 
     public async Task SendCustomEmail(User to, string subject, string message, EventStatusModel eventStatusModel = null, User replyTo = null)
     {
-        await _emailer.Send(
-            new EmailAddress(to.Email, to.FullName),
+        await SendEmailAsync(
+            to.Email, to.FullName,
             Replace(subject, to, eventStatusModel),
             Markdown.ToHtml(Replace(message, to, eventStatusModel)),
             replyTo != null ? new EmailAddress(replyTo.Email, replyTo.FullName) : null);
+    }
+    
+    public async Task SendEmailAsync(string toAddress, string toEmail, string subject, string message)
+    {
+        var emailMessage = new MimeMessage();
+        emailMessage.From.Add(new MailboxAddress(_configuration["SmtpSettings:SenderName"], _configuration["SmtpSettings:SenderEmail"]));
+        emailMessage.To.Add(new MailboxAddress(toAddress, toEmail));
+        emailMessage.Subject = subject;
+        emailMessage.Body = new TextPart("html") { Text = message };
+        using (var client = new SmtpClient())
+        {
+            await client.ConnectAsync(_configuration["SmtpSettings:Server"], int.Parse(_configuration["SmtpSettings:Port"]), MailKit.Security.SecureSocketOptions.StartTls);
+            await client.AuthenticateAsync(_configuration["SmtpSettings:Username"], _configuration["SmtpSettings:Password"]);
+            await client.SendAsync(emailMessage);
+            await client.DisconnectAsync(true);
+        }
     }
 
     private static string Replace(string value, User user, EventStatusModel model)
