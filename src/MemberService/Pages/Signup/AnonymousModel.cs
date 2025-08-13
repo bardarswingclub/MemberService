b9117a8cd6d824fd046f0eebd852e18effd1ab25
Frederik Vestre <freqmod@gmail.com>
2025-08-13 20:32:02 +0000

    Fix recapcha on signups from links

--- src/MemberService/Pages/Signup/AnonymousModel.cs
+++ src/MemberService/Pages/Signup/AnonymousModel.cs
@@ -1,8 +1,14 @@
 ﻿namespace MemberService.Pages.Signup;
+using Microsoft.Extensions.Options;
+using MemberService.Services;
 
 public class AnonymousModel
 {
+
     public Guid Id { get; init; }
     public string Title { get; init; }
     public string Description { get; init; }
+
+    public RecaptchaSettings recaptchaSettings { get; init; }
+    public string SiteKey => recaptchaSettings.SiteKey;
 }
